7be6091be2cff266b33aad08c482eeaadf951210
kianakarimi04 <kianakarimi8383@gmail.com>
2026-04-01 10:10:04 -0700

    Fix on utils.h, channels.c, messaging.c for freebsd handling

--- include/utils.h
+++ include/utils.h
@@ -4,6 +4,21 @@
 #include "client.h"
 #include <stddef.h>
 
+#if defined(__FreeBSD__)
+#include <sys/endian.h>
+#else
+#include <endian.h>
+#include <byteswap.h>
+#endif
+
+#ifndef htobe64
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#define htobe64(x) __bswap64(x)
+#else
+#define htobe64(x) (x)
+#endif
+#endif
+
 void cleanup_client(client_context *ctx);
 
 void print_usage(client_context *ctx);
